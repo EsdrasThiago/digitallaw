41d44d0c6b9650726e60df47e7c1db33dbb09abb
esdras <esdras12thiago@gmail.com>
2023-10-09 13:48:45 -0300

    Simulando perguntas e respostas

---
+++ src/mock/questions.js
@@ -0,0 +1,76 @@
+const questions = [
+  {
+    question: "pergunta",
+    correct_awnsner: 'correta',
+    wrong_awnsners: [
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+    ]
+  },
+  {
+    question: "pergunta",
+    correct_awnsner: 'correta',
+    wrong_awnsners: [
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+    ]
+  },
+  {
+    question: "pergunta",
+    correct_awnsner: 'correta',
+    wrong_awnsners: [
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+    ]
+  },
+  {
+    question: "pergunta",
+    correct_awnsner: 'correta',
+    wrong_awnsners: [
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+      {
+        wrong_awnser: 'wrong',
+      },
+    ]
+  },
+]
+
+export default questions;
