3c7abef423cb58877670767bb45580c410d01338
esdras <esdras12thiago@gmail.com>
2023-10-11 15:19:18 -0300

    Adicionando botao para levar ao jogo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom"
 import Header from "../components/Header"
 
 function Home() {
+
+  const navigate = useNavigate()
+  const quizButton = () => {
+    navigate("/game")
+  }
 
   return (
     <div>
       <Header />
       <h1>Home</h1>
+      <button onClick={quizButton}>Jogar Quiz</button>
     </div>
   )
 }
