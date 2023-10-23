9a24d4b7ca1b8e5cbb5f23db1aac1809db28a47a
esdras <esdras12thiago@gmail.com>
2023-10-23 15:14:46 -0300

    Fazendo Header e Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
+import facebook from "../images/facebook_icon.png"
+import instagram from "../images/instagram_icon.png"
+import linkedin from "../images/linkedin_icon.png"
+import twitter from "../images/twitter_icon.png"
+
 function Footer() {
 
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="main__footer">
+      <div className="icons__footer">
+        <img src={facebook} alt="icone do facebook" className="social__icons"/>
+        <img src={instagram} alt="icone do instagram" className="social__icons" />
+        <img src={linkedin} alt="icone do linkedin" className="social__icons" />
+        <img src={twitter} alt="icone do twitter" className="social__icons" />
+      </div>
+      <p>2023 Digit@l Law</p>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
+import facebook from "../images/facebook_icon.png"
+import instagram from "../images/instagram_icon.png"
+import linkedin from "../images/linkedin_icon.png"
+import twitter from "../images/twitter_icon.png"
+import digital from "../images/Logo_Digitl_Law.png"
+
 function Header() {
 
   return (
+    <div className="header__main">
+      <img src={digital} alt="logo da digital law" className="digital__icon" />
       <div>
-        <h1>Header</h1>
+        <img src={facebook} alt="icone do facebook" className="social__icons"/>
+        <img src={instagram} alt="icone do instagram" className="social__icons" />
+        <img src={linkedin} alt="icone do linkedin" className="social__icons" />
+        <img src={twitter} alt="icone do twitter" className="social__icons" />
       </div>
+    </div>
   )
 }
 
