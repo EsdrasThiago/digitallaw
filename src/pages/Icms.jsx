3bd65183b22b4b47066553f1fd65d6fe7747fe5b
esdras <esdras12thiago@gmail.com>
2023-11-09 13:21:40 -0300

    Arrumando alguns detalhes

--- src/pages/Icms.jsx
+++ src/pages/Icms.jsx
@@ -10,16 +10,16 @@ function Icms() {
   return (
     <div>
       <Header />
-      <div className="flex coming__class">
-        <Link to="/icsmdiferencial" className="icsm__link">
+      <div className="flex">
+        <Link to="/icsmdiferencial" className="icsm__link coming__class">
           <img src={diferencialIMG} alt="Saco de dinheiro escrito ICMS"/>
           <h2>O QUE SERIA O DIFERENCIAL DE ALÍQUOTAS DO ICMS-DIFAL</h2>
         </Link>
-        <Link to="/icsmimpactos" className="icsm__link">
+        <Link to="/icsmimpactos" className="icsm__link coming__class">
           <img src={impactoIMG} alt="Difal de ICMS: Empresas do Simples Nacional sao obrigadas a paga-lo?"/>
           <h2>IMPACTOS NO SIMPLES NACIONAL</h2>
         </Link>
-        <Link to="/icsmmodulacao" className="icsm__link">
+        <Link to="/icsmmodulacao" className="icsm__link coming__class">
           <img src={modulacaoIMG} alt="Diversas pessoas"/>
           <h2>MODULAÇÃO PROPOSTA PELO SUPERIOR TRIBUNAL FEDERAL REFERENTE AO JULGAMENTO DA AÇÃO DIRETA DE INCONSTITUCIONALIDADE A RESPEITO DESTA TEMÁTICA</h2>
         </Link>
